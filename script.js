f78a65ebea706a9d97da67e475bc04215e7a157e
OFRBG <fi@SaturnMx.local>
2018-07-24 18:59:11 -0500

    callback web3 accounts

--- script.js
+++ script.js
@@ -28,16 +28,23 @@ $(document).ready(function(){
     web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/metamask"));
   }
 
-  web3.eth.defaultAccount = web3.eth.accounts[0];
-  if(web3.eth.defaultAccount){
-    $('#web3-status').addClass('badge-success');
-    $('#web3-status').removeClass('badge-error');
-    $('#web3-status').text('web3 active');
-  } else {
-    $('#web3-status').addClass('badge-warning');
-    $('#web3-status').removeClass('badge-error');
-    $('#web3-status').text('web3 inactive');
-  }
+  web3.eth.getAccounts(function(e,v){
+    if(e) {
+      console.log(e)
+    } else {
+      web3.eth.defaultAccount = v[0];
+    }
+    
+    if(web3.eth.defaultAccount){
+      $('#web3-status').addClass('badge-success');
+      $('#web3-status').removeClass('badge-error');
+      $('#web3-status').text('web3 active');
+    } else {
+      $('#web3-status').addClass('badge-warning');
+      $('#web3-status').removeClass('badge-error');
+      $('#web3-status').text('web3 inactive');
+    }
+  });
 
   var CehhGold = web3.eth.contract(abi).at('0xAb8ea41e0D433E89fC4aa564ef46667c08587A2E');
 
